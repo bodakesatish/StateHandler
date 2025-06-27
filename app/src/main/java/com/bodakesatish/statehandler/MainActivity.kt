cca722c5460a23b011c571f9d220ef899d4b131a
Satish Bodake <satishbodake@Satishs-Mac-mini.local>
2025-06-27 15:45:55 +0530

    Initial commit

---
+++ app/src/main/java/com/bodakesatish/statehandler/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.bodakesatish.statehandler
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bodakesatish.statehandler.ui.theme.StateHandlerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StateHandlerTheme {
+                PeopleScreenRoot()
+            }
+        }
+    }
+}
